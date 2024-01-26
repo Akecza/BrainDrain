d2ef9bb772919ffdd625538701db93e1e4bc4653
Krzysztof Popieluch <k.popieluch@visiona.pl>
2024-01-26 19:47:21 +0100

    Player

---
+++ scenes/player.gd
@@ -0,0 +1,28 @@
+extends Node2D
+@export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("walk_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("walk_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("walk_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("walk_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
