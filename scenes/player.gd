ddfb6d9731ee376c98ef4b9d4abcd06fb029ba73
Krzysztof Popieluch <k.popieluch@visiona.pl>
2024-01-26 19:51:50 +0100

    Zmniejszylam players

--- scenes/player.gd
+++ scenes/player.gd
@@ -4,7 +4,7 @@ var screen_size # Size of the game window.
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
