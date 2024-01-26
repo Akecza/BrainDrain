c49099de9319c6b07f274d4e9b71f1dc5a2e8805
Krzysztof Popieluch <k.popieluch@visiona.pl>
2024-01-26 23:35:03 +0100

    uhuii

--- scenes/cake.gd
+++ scenes/cake.gd
@@ -4,7 +4,7 @@ var speed = 300
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,5 +13,12 @@ func _process(delta):
 
 
 func _on_body_entered(body):
-	if body.is_in_group("SadMen"):
-		queue_free()
+	if body.is_in_group("Cakeables"):
+		$HappyArea.disabled=false
+	$HappyTimer.start()
+	
+
+
+
+func _on_happy_timer_timeout():
+	queue_free()
