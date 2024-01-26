7fbf87144b672cc1642e19838d893825057deb41
Krzysztof Popieluch <k.popieluch@visiona.pl>
2024-01-27 00:13:34 +0100

    buttons

---
+++ scenes/button.gd
@@ -0,0 +1,16 @@
+extends Button
+
+@export var input : String
+
+func _input(event):
+	if (event.is_action_pressed(input)):
+		emit_signal("button_down")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
