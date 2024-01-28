df8a3a348c88aa166a40214c8c9f0dc5f458775e
Krzysztof Popieluch <k.popieluch@visiona.pl>
2024-01-28 10:10:25 +0100

    fast scene load

--- scenes/PlayButton.gd
+++ scenes/PlayButton.gd
@@ -1,15 +1,21 @@
 extends Button
 
+var game_scene = load("res://scenes/game.tscn")
+
+
 func start_game():
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	#get_tree().change_scene_to_file("res://scenes/game.tscn")
+	get_tree().change_scene_to_packed(game_scene)
 	
 func exit_game():
 	get_tree().quit()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
+	#game_scene = preload("res://scenes/game.tscn")
+	#var new_scene = game_scene.instantiate()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
